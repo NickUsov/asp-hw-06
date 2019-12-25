9e1683d205e3d7446e9e0ae9349287873eb8fe1a
Nick Usov <nicky.yalta.ua@gmail.com>
2019-12-25 16:59:50 +0200

    model QuestRoom

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication11.Models;
+
+namespace WebApplication11.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Room room = new Room { Address = "Mira, 25-B", Company = "ITStep", Descript = "Exersize room", Email = "itstep@gmail.org", Fear = 5, Logo = "path_to_logo", MaxPlayers = 20, MinPlayers = 1, MinYears = 16, Phones = "223-322-323-232", Rate = 12.0, Time = "00:05:00", Title = "Room #9", Сomplexity = 5 };
+
+            return View(room);
+        }
+    }
+}
---
+++ Models/Room.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication11.Models
+{
+    public class Room
+    {
+        public string Title { get; set; }
+        public string Descript { get; set; }
+        public string Time { get; set; }
+        public int MinPlayers { get; set; }
+        public int MaxPlayers { get; set; }
+        public int MinYears { get; set; }
+        public string Address { get; set; }
+        public string Phones { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public double Rate { get; set; }
+        public int Fear { get; set; }
+        public int Сomplexity { get; set; }
+        public string Logo { get; set; }
+    }
+}
